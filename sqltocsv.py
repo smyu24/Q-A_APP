93d7d2a418d953d4f78f37b011a8a708fc33f35d
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2023-05-30 12:50:33 -0700

    Delete sqltocsv.py

--- sqltocsv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import sqlite3
-
-conn = sqlite3.connect("satsuite.sqlite", isolation_level=None,
-                       detect_types=sqlite3.PARSE_COLNAMES)
-db_df = pd.read_sql_query("SELECT * FROM questionDetails WHERE section='Math'", conn)
-db_df.to_csv('database.csv', index=False)
