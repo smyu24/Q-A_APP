4206c7f5346d6487046052d338df182f72371f0b
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2023-06-06 10:39:19 -0700

    Update sqlMigrateViaCSV.py

--- sqlMigrateViaCSV.py
+++ sqlMigrateViaCSV.py
@@ -22,9 +22,10 @@ cursor = mydb.cursor()
 csv_data = csv.reader('database.csv')
 for row in csv_data:
 
-    cursor.execute('INSERT INTO questionDetails (item_id,section,prompt,body,passage_directions,passage_attribution,passage_body,style,correct_choice,rationale,a,b,c,d)' \
-          'VALUES("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', 
-          row)
+    #cursor.execute('INSERT INTO questionDetails (item_id,section,prompt,body,passage_directions,passage_attribution,passage_body,style,correct_choice,rationale,a,b,c,d) VALUES({0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13})'.format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13])) # (:item_id,:section,:prompt,:body,:passage_directions,:passage_attribution,:passage_body,:style,:correct_choice,:rationale,:a,:b,:c,:d)
+    cursor.execute("INSERT INTO questionDetails (item_id,section,prompt,body,passage_directions,passage_attribution,passage_body,style,correct_choice,rationale,a,b,c,d) VALUES (:item_id,:section,:prompt,:body,:passage_directions,:passage_attribution,:passage_body,:style,:correct_choice,:rationale,:a,:b,:c,:d)",
+                            {'item_id': row[0], 'section': row[1], 'prompt': row[2], 'body': row[3], 'passage_directions': row[4], 'passage_attribution': row[5], 'passage_body': row[6], 'style': row[7],
+                             'correct_choice': row[8], 'rationale': row[9], 'a': row[10], 'b': row[11], 'c': row[12], 'd': row[13]})
 
 mydb.commit()
 cursor.close()
